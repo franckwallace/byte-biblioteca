a9feadd98bf3378aed968ddd19e8258e366349ca
Franck Wallace <franckwallace.fw@outlook.com>
2025-02-04 22:57:12 -0300

    Implementação de classes no diretório 'Usuários'

---
+++ ByteBiblioteca/Usuários/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBiblioteca.Usuários
+{
+    internal class Usuario
+    {
+        public string Nome {  get; set; }
+        public string CPF { get; set; }
+
+
+    }
+}
